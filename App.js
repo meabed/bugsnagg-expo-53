f1e1c311e726a8c0a526ac92d23be654b2729c81
Mohamed Meabed <mo.meabed@gmail.com>
2025-05-22 09:36:00 -0400

    chore: update

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Bugsnag from "@bugsnag/expo";
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import * as React from 'react';
+
+Bugsnag.start({
+    apiKey: '123'
+})
+
+const ErrorBoundary = Bugsnag.getPlugin('react').createErrorBoundary(React)
+
+const ErrorView = () =>
+    <View>
+        <Text>Inform users of an error in the component tree.</Text>
+    </View>
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <ErrorBoundary FallbackComponent={ErrorView}>
+            <View style={styles.container}>
+                <Text>Open up App.js to start working on your app!</Text>
+                <StatusBar style="auto"/>
+            </View>
+        </ErrorBoundary>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
